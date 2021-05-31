a10bccc1c6e323a75323013a856cfd7b2147a094
Greg <caldwell.gregoryj@gmail.com>
2021-05-31 11:51:47 -0400

    started to create react file

--- phase-2-project/src/App.js
+++ phase-2-project/src/App.js
@@ -1,25 +1,37 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Categories from './Components/Categories';
+import UserLogin from './Components/UserLogin';
+import Search from './Components/Search';
+import GenerateLists from './Components/GenerateLists';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  componentDidMount(){
+    fetch("http://localhost:3000/pokemon")
+    .then(res => res.json())
+    .then(pokemonArr => {
+      this.setState({
+        pokemons: pokemonArr
+      })
+    })
+  }
+
+  render () {
+   
+    return (
+      <div classname="App">
+        <UserLogin />
+        <Categories />
+        <Search />
+        <GenerateLists />
+      </div>
+    )
+  }
+
 }
+
 
 export default App;
